0c85850dfb03d9109094cd61878fee6be8c72de7
vrodriguezf <victor.fernandez@upm.es>
2024-06-10 03:52:40 +0200

    first try

--- exam_sort_ocr/_modidx.py
+++ exam_sort_ocr/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://vrodriguezf.github.io',
                 'git_url': 'https://github.com/vrodriguezf/exam-sort-ocr',
                 'lib_path': 'exam_sort_ocr'},
-  'syms': {'exam_sort_ocr.core': {'exam_sort_ocr.core.foo': ('core.html#foo', 'exam_sort_ocr/core.py')}}}
+  'syms': { 'exam_sort_ocr.core': { 'exam_sort_ocr.core.encode_image': ('core.html#encode_image', 'exam_sort_ocr/core.py'),
+                                    'exam_sort_ocr.core.extract_info': ('core.html#extract_info', 'exam_sort_ocr/core.py'),
+                                    'exam_sort_ocr.core.num_tokens_from_messages': ( 'core.html#num_tokens_from_messages',
+                                                                                     'exam_sort_ocr/core.py')}}}
